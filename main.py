a759efbe7f5398f5688ae2bca49e462b573fab36
Adriana Zubieta <a.zubieta.z@gmail.com>
2020-03-22 18:06:50 -0400

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import pandas
-import statistics as st
-daily_report = pandas.read_csv('csse_covid_19_data/csse_covid_19_daily_reports/03-18-2020.csv')
- 
-for index, row in daily_report.iterrows():
-    print("{} country: {} - Deaths {}".format(index, row['Country/Region'], row['Deaths']))
-
-
